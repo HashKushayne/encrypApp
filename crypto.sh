a6fe98281d888ae988c221420e9c7d0a25393542
HashKushayne <hashitha@live.com>
2017-04-25 17:07:22 +0530

    Add files via upload

---
+++ crypto.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+
+echo
+echo "Welcome to Encrypt App"
+echo 
+
+echo "What is the file? "
+read efile
+
+	openssl rsautl -encrypt -pubin -inkey public.pem -keyform pem -in $efile -out $efile.enc
+	rm $efile
+	mv $efile.enc $efile
+
+
+
+
+
+
+
+
+
---
+++ crypto2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+
+echo
+echo "Welcome to Decrypt App"
+echo
+
+echo "What is the file? "
+read efile
+
+	openssl rsautl -decrypt -inkey private.pem -keyform pem -in $efile -out $efile.dec
+	rm $efile
+	mv $efile.dec $efile
+
+
