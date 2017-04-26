4c402803966f3046bf3610fb584c1bd8d8764b4d
HashKushayne <hashitha@live.com>
2017-04-26 11:00:50 +0530

    Update d.sh

--- d.sh
+++ d.sh
@@ -11,6 +11,7 @@ echo "What is the file you wanted decrypted? "
 read efile
 
 #This is where the decryption happens
+#Decryption is done by private key, hence only the users that posses private key "private.pem" can decrypt.
 	openssl rsautl -decrypt -inkey private.pem -keyform pem -in $efile -out $efile.dec
 	rm $efile
 	mv $efile.dec $efile
