838003dbe5d20fadcf506763376f789db173e326
HashKushayne <hashitha@live.com>
2017-04-26 06:20:49 +0530

    Update encryptApp.sh

--- encryptApp.sh
+++ encryptApp.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+#This is the encryption/decryption app
+
 
 echo
 echo "Welcome!"
 echo
 
+#ask what you want encrytion or decryption and takes to corresponding task according to our reply "e" or "d"
 echo "What do you want to do [(e)ncrypt/(d)ecrypt]?"
 read answer
 ./$answer.sh
