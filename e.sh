0f717da85044a31c46689c7b3554249587b12659
HashKushayne <hashitha@live.com>
2017-04-26 06:23:52 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -6,9 +6,11 @@ echo
 echo "Welcome to Encrypt App"
 echo 
 
+#Ask for the file name to be encrypted and getting the input
 echo "What is the file you want encrypted? "
 read efile
 
+#This is where the encryption happens
 	openssl rsautl -encrypt -pubin -inkey public.pem -keyform pem -in $efile -out $efile.enc
 	rm $efile
 	mv $efile.enc $efile
